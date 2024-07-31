6c86f3ddfa7631ec0e1608aa1fc9fa99cbda1b67
phanvanhai <phanvanhaixdddk49@gmail.com>
2024-07-31 16:44:06 +0700

    up

--- data/user.ts
+++ data/user.ts
@@ -2,7 +2,7 @@ import { db } from "@/lib/db";
 
 export const getUserByEmail = async (email: string) => {
   try {
-    const user = await db.User.findUnique({
+    const user = await db.user.findUnique({
       where: {
         email,
       },
@@ -16,7 +16,7 @@ export const getUserByEmail = async (email: string) => {
 
 export const getUserById = async (id: string) => {
   try {
-    const user = await db.User.findUnique({
+    const user = await db.user.findUnique({
       where: {
         id,
       },
